126363d16407765090637daaf0b896adbd59ffdf
martincss <martincarusso@gmail.com>
2017-08-25 00:08:07 -0300

    agregado el ejercicio 2 de la guia 1

---
+++ guia_1/ej2.py
@@ -0,0 +1,57 @@
+import numpy as np
+from numpy import pi
+import matplotlib.pyplot as plt
+
+def CD(x, delta_x):
+	"""
+	Hace la diferencia central para cdc periodicas
+	"""
+	diff = (np.roll(x,-1) - np.roll(x,+1))/(2*delta_x)
+	return diff
+	
+def CD2(x, delta_x):
+	"""
+	Hace la segunda derivada para cdc periodicas 
+	""" 
+	diff2 = (np.roll(x,+1) - 2*x + np.roll(x,-1))/(delta_x**2) 
+	return diff2
+
+def F(x, delta_x, nu):
+	f = nu*CD2(x,delta_x) - x*(CD(x,delta_x))
+	return f
+
+def RK2(x, delta_x, delta_t, nu):
+	x_star = x + delta_t/2 * F(x, delta_x, nu)
+	x_adv = x + delta_t * F(x_star, delta_x, nu)
+	return x_adv 
+
+x_min = 0
+x_max = 2*pi
+delta_x = 0.2
+X = np.arange(x_min, x_max, delta_x)
+
+t_inicial = 0
+t_final = 5
+delta_t = 0.15
+T = np.arange(t_inicial, t_final, delta_t)
+
+nu = 0.1
+
+u = np.zeros((len(T), len(X)))
+
+u[0,:] = np.sin(X)
+
+for i in range(len(T)-2):
+	u[i+1,:] = RK2(u[i,:], delta_x, delta_t, nu)
+
+plt.figure()
+for i in range(len(T)-2):
+	if (i%2 == 0):
+		plt.plot(X, u[i,:], label = 't = {:.2f}'.format(T[i]))
+plt.grid(True)
+plt.legend()
+plt.show()
+
+
+
+
