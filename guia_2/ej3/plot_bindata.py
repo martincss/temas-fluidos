327715ded986000b9c2c68b8fe8b443a43e41b8a
martincss <martincarusso@gmail.com>
2017-10-06 02:07:04 -0300

    retirados los archivos de datos

---
+++ guia_2/ej3/plot_bindata.py
@@ -0,0 +1,35 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Reads a binary file and plots a cut in the x-y plane.
+# Execute in ipython with '%run plot_bindata.py'
+
+# Path to the binary data
+path = './ej3/'
+
+# Spatial resolution
+NX = 128
+NY = 128
+NZ = 128
+shape = (NX,NY,NZ)
+
+# Reads binary files
+#vx = np.fromfile(path+'vx.0001.out',dtype=np.float32).reshape(shape,order='F')
+
+# Show a horizontal cut of the field in the middle of the box
+# plt.figure(1)
+# plt.imshow(vx[:,:,NZ/2])
+# plt.xlabel('x')
+# plt.ylabel('y')
+# plt.show()
+plt.figure()
+for i in range(1,21):
+    vx = np.fromfile(path+'vx.{:04d}.out'.format(i),dtype=np.float32).reshape(shape,order='F')
+    plt.clf()
+    plt.title('i = {:}'.format(i))
+    plt.imshow(vx[:,:,NZ//2], cmap = 'jet')
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.colorbar()
+    plt.pause(1)
+plt.show()
