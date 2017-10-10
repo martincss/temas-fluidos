340ff2ae760d6f740994d3f0573c95aa75eba6ea
Martin <martincarusso@gmail.com>
2017-10-09 23:38:17 -0300

    agregados las condiciones iniciales y parametros para reproducir el ej3, y el script de analisis preliminar

--- guia_2/ej3/plot_bindata.py
+++ guia_2/ej3/plot_bindata.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 # Execute in ipython with '%run plot_bindata.py'
 
 # Path to the binary data
-path = './ej3/'
+path = './omega_10'
 
 # Spatial resolution
 NX = 128
@@ -25,9 +25,17 @@ shape = (NX,NY,NZ)
 plt.figure()
 for i in range(1,21):
     vx = np.fromfile(path+'vx.{:04d}.out'.format(i),dtype=np.float32).reshape(shape,order='F')
+    vy = np.fromfile(path+'vy.{:04d}.out'.format(i),dtype=np.float32).reshape(shape,order='F')
+    vz = np.fromfile(path+'vz.{:04d}.out'.format(i),dtype=np.float32).reshape(shape,order='F')
+
+    wx = np.fromfile(path+'wx.{:04d}.out'.format(i),dtype=np.float32).reshape(shape,order='F')
+    wy = np.fromfile(path+'wy.{:04d}.out'.format(i),dtype=np.float32).reshape(shape,order='F')
+    wz = np.fromfile(path+'wz.{:04d}.out'.format(i),dtype=np.float32).reshape(shape,order='F')
+
+    h = vx*wx + vy*wy + vz*wz
     plt.clf()
     plt.title('i = {:}'.format(i))
-    plt.imshow(vx[:,:,NZ//2], cmap = 'jet')
+    plt.imshow(h[:,NY//2,:], cmap = 'jet', vmin = -15, vmax = 15)
     plt.xlabel('x')
     plt.ylabel('y')
     plt.colorbar()
