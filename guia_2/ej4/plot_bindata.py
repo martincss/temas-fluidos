e1ed07d65d5f5049979bfa41e806663b4a1c381e
Martin Carusso <martinc@lpc11.labs.df.uba.ar>
2017-10-12 21:49:52 -0300

    empezado el analisis del cuatro, estan los datos de varias N

--- guia_2/ej4/plot_bindata.py
+++ guia_2/ej4/plot_bindata.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
 
 # Reads a binary file and plots a cut in the x-y plane.
 # Execute in ipython with '%run plot_bindata.py'
@@ -13,24 +14,45 @@ NY = 128
 NZ = 64
 shape = (NX,NY,NZ)
 
-# Reads binary files
-#vx = np.fromfile(path+'vx.0001.out',dtype=np.float32).reshape(shape,order='F')
+x = np.linspace(0, 2*np.pi, NX)
 
-# Show a horizontal cut of the field in the middle of the box
-# plt.figure(1)
-# plt.imshow(vx[:,:,NZ/2])
-# plt.xlabel('x')
-# plt.ylabel('y')
-# plt.show()
+#%% plot del colormap
+"""
 plt.figure()
-for i in range(1,9):
-    vx = np.fromfile(path+'vz.{:04d}.out'.format(i),dtype=np.float32).reshape(shape,order='F')
+for i in range(1,12):
+    field = np.fromfile(path+'th.{:04d}.out'.format(i),dtype=np.float32).reshape(shape,order='F')
     plt.clf()
     plt.title('i = {:}'.format(i))
-    plt.imshow(vx[:,NY//2,:], cmap = 'jet')#, vmin = , vmax = )
+    plt.imshow(np.transpose(field[:,NY//2,:]), cmap = 'jet', origin = 'lower')#, vmin = , vmax = )
     plt.xlabel('x')
-    plt.ylabel('y')
+    plt.ylabel('z')
     plt.colorbar()
     plt.pause(1)
-    print(np.max(vx))
 plt.show()
+"""
+#%% plot de temp(x)
+
+plt.figure()
+for i in range(1,2):
+    field = np.fromfile(path+'th.{:04d}.out'.format(i),dtype=np.float32).reshape(shape,order='F')
+    plt.clf()
+    plt.title('i = {:}'.format(i))
+    plt.plot(x, field[:, NY//2, NZ//2])
+    plt.xlabel('x')
+    plt.ylabel('temp')
+    plt.grid(True)
+    plt.pause(1)
+#plt.show()
+
+#%% Fitteo
+x_fit = x[:20]
+temp_fit = field[:20, NY//2, NZ//2]
+f = lambda x, A, lon, ph: A*np.sin(lon*x+ph)
+
+popt, _ = curve_fit(f, x_fit, temp_fit)
+fit = f(x_fit, *popt)
+
+plt.plot(x_fit, fit, 'r')
+plt.plot(x_fit, temp_fit, 'b')
+plt.show()
+
