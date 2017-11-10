df41c7a40d266070d6da26325ea6b278ef4cc365
Martin Carusso <martinc@lpc10.labs.df.uba.ar>
2017-11-09 21:48:59 -0300

    en el ej2, ahora entendimos un toque más la clase, y que no hace falta usar el callable, puede ser un return de integrate

--- guia_3/ej2/lorenz.py
+++ guia_3/ej2/lorenz.py
@@ -14,17 +14,18 @@ from scipy.integrate import ode
 
 #%% Definimos funciones para la integración
 
-def F(X, sigma, r, b):
+def F(t, X, sigma, r, b):
     """
     Dado el array (X,Y,Z) y los parámetros, devuelve 
     el array con las derivadas de (X,Y,Z) según las 
     ecuaciones de Lorenz 63
     """
+    #X = np.array(X)
     DX = np.zeros(len(X))
 
     DX[0] = sigma*(X[1] - X[0])
     DX[1] = -X[0]*X[2] + r*X[0] - X[1]
-    DX[2] = X[0]*X[1] - B*X[2]
+    DX[2] = X[0]*X[1] - b*X[2]
     
     return DX
 
@@ -38,21 +39,40 @@ list_sol = []
 
 # necesitamos un callable para que reciba las
 # soluciones del solver.integrate
-def call(t, x):
-    list_sol.append([t, *x])
+#def call(t, x):
+#    list_sol.append([t, *x])
 
-solver.set_solout(call)
-solver.set_initial_value()
+#solver.set_solout(call)
 
 # meter el sigma, b y r
-params = [0, 0, 0]
+
+sigma = 10 
+r = 2
+b = 8/3
+params = [sigma, r, b]
+
+t0 = 0 # inicial
+t1 = 50 # final, parece que con este solver no hay
+	# que poner el paso
+W_0 = np.array([0, 0.5, 0.5])
+
 
 #://stackoverflow.com/questions/12926393/using-adaptive-step-sizes-with-scipy-integrate-ode
 
+# usamos methods en la clase ode para poner 
+# los valores iniciales y parámetros en la
+# función derivada
 
-def Lorenz(solver)
+solver.set_initial_value(W_0, t0)
+solver.set_f_params(*params)
+w = solver.integrate(t1)
+
+# Pasamos la lista de soluciones a un array
+#list_sol = np.array(list_sol)
+
+def Lorenz(solver):
     pass
 
 
 
-
+ 
