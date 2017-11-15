8d07bb1bbaf7fca72c63599df323ea1ba6137a24
martincss <martincarusso@gmail.com>
2017-11-14 23:20:16 -0300

    emprolijado lorenz, ya está implementado como una funcion para parametros y CI variables, y un array de tiempos cualesquiera

--- guia_3/ej2/lorenz.py
+++ guia_3/ej2/lorenz.py
@@ -4,14 +4,17 @@ Martín Carusso, Florencia Lazzari
 Temas avanzados de dinámica de fluidos, 2c 2017
 Guía 3
 
-Programa para la solución numérica del sistema Lorenz 63
+Programa para la solución numérica del sistema Lorenz '63
 
 """
 
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.integrate import ode
 from mpl_toolkits.mplot3d import Axes3D
+from scipy.integrate import ode
+# Ejemplo de uso de la clase 'ode'
+#://stackoverflow.com/questions/12926393/using-adaptive-step-sizes-with-scipy-integrate-ode
+
 
 #%% Definimos funciones para la integración
 
@@ -30,13 +33,19 @@ def F(t, X, sigma, r, b):
     
     return DX
 
+
 # Esto es una instance de la class ode, con el 
 # parametro dopri5 en el method set_integrator
 solver = ode(F).set_integrator('dopri5')
 
+"""
+El solver no requiere que se especifique un paso, se puede usar un callable
+para que devuelva los valores a cada paso intermedio del integrate, como 
+hicimos abajo
+"""
 # acá van a venir las soluciones a distintos
 # tiempos
-list_sol = []
+#list_sol = []
 
 # necesitamos un callable para que reciba las
 # soluciones del solver.integrate
@@ -45,41 +54,50 @@ list_sol = []
 
 #solver.set_solout(call)
 
-# meter el sigma, b y r
+# Se pasan los parámetros, la condición inicial y se integra el sistema
 
+# Pasamos la lista de soluciones a un array
+#list_sol = np.array(list_sol)
+"""
+En lugar de usar el callable para recibir la solución a cada paso intermedio, 
+para poder controlar 'manualmente' la densidad de puntos, podemos pedir un
+return de solver.integrate(t), para cada t de un array. Claramente es más 
+lento, pero a nuestros efectos no es tan relevante como poder elegir la 
+densidad de puntos.
+"""
+
+# meter el sigma, b y r
 sigma = 10 
-r = 2
+r = 30
 b = 8/3
 params = [sigma, r, b]
 
 t0 = 0 # inicial
-t1 = 50 # final, parece que con este solver no hay
-	# que poner el paso
+t1 = 50 # final, parece que con este solver no hay que poner el paso
 W_0 = np.array([0, 0.5, 0.5])
 
 
-#://stackoverflow.com/questions/12926393/using-adaptive-step-sizes-with-scipy-integrate-ode
 
-# usamos methods en la clase ode para poner 
-# los valores iniciales y parámetros en la
-# función derivada
-
-solver.set_initial_value(W_0, t0)
-solver.set_f_params(*params)
-w = solver.integrate(t1)
-
-# Pasamos la lista de soluciones a un array
-#list_sol = np.array(list_sol)
-
-def Lorenz(solver):
-    pass
-
-tiempos = np.linspace(t0, t1, 100)
-sol = np.zeros((len(tiempos), 3))
+def Lorenz(solver, params, W_0, tiempos):
+    """
+    Dado el solver para F, la lista de parámetros (sigma, r, b), la condición 
+    inicial W_0, y un array de tiempos; integra el sistema Lorenz '63 para 
+    cada tiempo del array.
+    """
+    # usamos methods en la clase ode para poner 
+    # los valores iniciales y parámetros en la
+    # función derivada
+    solver.set_initial_value(W_0, tiempos[0])
+    solver.set_f_params(*params)
+    
+    sol = np.zeros((len(tiempos), 3))
+    for i in range(len(tiempos)):
+        sol[i, :] = solver.integrate(tiempos[i])
+    return sol
+   
+tiempos = np.linspace(t0, t1, 10000)
+sol = Lorenz(solver, params, W_0, tiempos)
 
 ax3 = Axes3D(plt.figure())
-
-for i in range(len(tiempos)):
-    sol[i, :] = solver.integrate(tiempos[i])
 ax3.plot(sol[:,0], sol[:,1], sol[:,2])    
  
